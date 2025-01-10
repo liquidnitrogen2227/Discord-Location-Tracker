1597baac95791dfdb1adf44831b052e782f0e872
Pranav <venkatasaipranav2227@gmail.com>
2025-01-10 12:38:40 +0530

    Update

--- script.js
+++ script.js
@@ -11,49 +11,49 @@ const CHECKPOINTS = [
         name: "Home",
         lat: 12.911181,   // Replace with actual latitude
         lng: 77.551658,  // Replace with actual longitude
-        radius: 50,  // 50 meters radius
+        radius: 100,  // 50 meters radius
         passed: false  // To track if checkpoint has been passed
     },
     {
         name: "Sarakki",
         lat: 12.906528, 
         lng: 77.574605,
-        radius: 50,
+        radius: 100,
         passed: false
     },
     {
         name: "McD",
         lat: 12.907123,
         lng: 77.585674,
-        radius: 50,
+        radius: 100,
         passed: false
     },
     {
         name: "TTD",
         lat: 12.910535, 
         lng: 77.588113,
-        radius: 50,
+        radius: 100,
         passed: false
     },
     {
         name: "MG",
         lat: 12.910332,
         lng:  77.594900,
-        radius: 50,
+        radius: 100,
         passed: false
     },
     {
         name: "MINI-FOREST",
         lat: 12.910627,
         lng:  77.598852,
-        radius: 50,
+        radius: 100,
         passed: false
     },
     {
         name: "VEGA SIGNAL",
         lat: 12.907040, 
         lng: 77.599499,
-        radius: 50,
+        radius: 100,
         passed: false
     },
 ];
